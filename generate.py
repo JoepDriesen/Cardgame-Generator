8816c488714fd6af3bbc0135c62360d308c7b8cd
Joep Driesen <joep@seasoning.be>
2015-02-05 17:05:20 +0100

    added mandatory and status cards

--- generate.py
+++ generate.py
@@ -20,19 +20,26 @@ def generate():
     parser.add_argument('-w', '--width', action='store', type=int, default=DEFAULT_WIDTH, help='The width of the generated cards in px. Default: {}'.format(DEFAULT_WIDTH))
     parser.add_argument('--height', action='store', type=int, default=DEFAULT_HEIGHT, help='The height of the generated cards in px. Default: {}'.format(DEFAULT_HEIGHT))
     parser.add_argument('-f', '--font', action='store', default=DEFAULT_FONT, help='The font to use for card generation. A corresponding font file must be present in \'assets/fonts\'. Default: {}'.format(DEFAULT_FONT))
+    parser.add_argument('-c', '--card', action='store', default=None, help='If this option is provided, only the card with the given name will be generated.')
     args = parser.parse_args()
     
     card_types = {card_type.name.lower(): card_type for card_type in parse_card_types()}
     
     cards_generated = {}
     for card in parse_cards(card_types=card_types, lang=args.lang):
-        assemble_card(card, card_width=args.width, card_height=args.height, font=get_font(args.font))
-        cards_generated[card.type.name] = cards_generated.get(card.type.name, 0) + 1
+        if args.card is None or card.name.lower() == args.card.lower():
+            assemble_card(card, card_width=args.width, card_height=args.height, font=get_font(args.font))
+            cards_generated[card.type.name] = cards_generated.get(card.type.name, 0) + 1
     
     # Display the statistics on generated cards
-    print('Cards generated:')
+    print('Cards generated:\n')
     for generated_type in sorted(cards_generated.keys()):
-        print('{}: {}'.format(generated_type, cards_generated[generated_type]))
-        
+        print('{:15s} {:>3}'.format(generated_type, cards_generated[generated_type]))
+    
+    if len(cards_generated) <= 0:
+        print('None')
+    else:
+        print('-'*20)
+        print('{:15s} {:>3}'.format('Total', sum(cards_generated.values())))
 if __name__ == '__main__':
     generate()
